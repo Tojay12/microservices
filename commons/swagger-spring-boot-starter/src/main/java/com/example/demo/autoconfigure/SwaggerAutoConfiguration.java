0e6377cc3ac9f142dc85973048bb6dd1b3151c64
jyw <jiangyuanwei@honezhi.com>
2020-10-13 10:19:09 +0800

    修改自定义得文件是否开启

--- commons/swagger-spring-boot-starter/src/main/java/com/example/demo/autoconfigure/SwaggerAutoConfiguration.java
+++ commons/swagger-spring-boot-starter/src/main/java/com/example/demo/autoconfigure/SwaggerAutoConfiguration.java
@@ -54,7 +54,7 @@ public class SwaggerAutoConfiguration implements BeanFactoryAware {
 
     @Bean
     @ConditionalOnMissingBean
-    @ConditionalOnProperty(name = "zlt.swagger.enabled", matchIfMissing = true)
+    @ConditionalOnProperty(name = "swagger.enabled", matchIfMissing = true)
     public List<Docket> createRestApi(SwaggerProperties swaggerProperties) {
         ConfigurableBeanFactory configurableBeanFactory = (ConfigurableBeanFactory) beanFactory;
         List<Docket> docketList = new LinkedList<>();
